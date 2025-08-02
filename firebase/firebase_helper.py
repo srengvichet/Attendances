e626ea0ed259af04e2b85f2520274bf3c8353bbf
vichet sreng <sreng.vichet@gmail.com>
2025-08-02 04:11:54 +0000

    update requirements dependance

--- firebase/firebase_helper.py
+++ firebase/firebase_helper.py
@@ -5,7 +5,7 @@ from firebase_admin import credentials, db
 from datetime import datetime
 
 # Initialize Firebase (run once)
-cred = credentials.Certificate("firebase_config.json")
+cred = credentials.Certificate("firebase/firebase_config.json")
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://sampleapp-1fb9a-default-rtdb.asia-southeast1.firebasedatabase.app/',  # 🔁 Replace this
